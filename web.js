a036263c1d0b4f465a759c446c5e874cf065e98f
antimatter15 <antimatter15@gmail.com>
2012-07-31 11:24:21 -0400

    really the end

--- web.js
+++ web.js
@@ -19,8 +19,8 @@ app = express.createServer(express.logger());
 io = require('socket.io').listen(app);
 
 io.configure(function() {
+  io.set("polling duration", 10);
   io.set("log level", 2);
-  io.set("connect timeout", 2000);
   return io.set("authorization", function(data, fn) {
     var cookie;
     if (!data.headers.cookie) {
